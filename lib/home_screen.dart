9cfe8874a84fc35c6eb138cfb95c8f687a8bb432
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-03 14:37:09 +0300

    final version

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,8 +11,59 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _loading = true;
   File _image;
-  List _ouptut;
+  List _output;
   final picker = ImagePicker();
+
+  @override
+  void initState() { 
+    super.initState();
+    loadModel();
+  }
+
+  detectImage(File image) async {
+      var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 8,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+
+        setState(() {
+                  _output = output;
+                  _loading = false;
+                });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(model: 'assets/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  @override
+    void dispose() {
+      // TODO: implement dispose
+      super.dispose();
+    }
+
+    pickImage() async {
+      var image = await picker.getImage(source: ImageSource.camera);
+      if (image == null) return null;
+      setState(() {
+              _image = File(image.path);
+            });
+
+            detectImage(_image);
+    }
+
+    pickGalleryImage() async {
+      var image = await picker.getImage(source: ImageSource.gallery);
+      if(image == null) return null;
+
+      setState(() {
+              _image = File(image.path);
+            });
+
+            detectImage(_image);
+    }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,14 +96,75 @@ class _HomePageState extends State<HomePage> {
                 height: 50,
               ),
               Center(
-                child: _loading ? Container(
-                  width: 350,
-                  child: Column(
-                    children: [
-                      Image.asset('assets/marvel1.png')
-                    ],
-                  ),
-                ) : Container(),
+                child: _loading
+                    ? Container(
+                        width: 350,
+                        child: Column(
+                          children: [
+                            Image.asset('assets/marvel1.png'),
+                            SizedBox(height: 50),
+                          ],
+                        ),
+                      )
+                    : Container(
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 250,
+                              child: Image.file(_image),
+                            ),
+                            SizedBox(height: 20),
+                            _output != null
+                                ? Text(
+                                    '${_output[0]['label']}',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                : Container(),
+                            SizedBox(
+                              height: 10,
+                            )
+                          ],
+                        ),
+                      ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        pickImage();
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 250,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(6.0)
+                        ),
+                        child: Text('Capture image using Camera', style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                     GestureDetector(
+                      onTap: () {
+                        pickGalleryImage();
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width - 250,
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                          borderRadius: BorderRadius.circular(6.0)
+                        ),
+                        child: Text('Pick Image from Gallery', style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
