b2bd5829f6b7fac0064e894956ef88e72c65b287
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-06-04 21:49:50 +0300

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Marvel App',
-      home: IntroScreen(),
+      home: IntroScreen(),e
     );
   }
 }
